d97e03bf50acd0bcc00fca266dc859fa0f0d51c7
Nathan Mares <ngmares@gmail.com>
2021-10-27 03:20:31 +0000

    MDL-72405 Rework the activity completion cron query to improve performance

--- completion/criteria/completion_criteria_activity.php
+++ completion/criteria/completion_criteria_activity.php
@@ -205,6 +205,9 @@ class completion_criteria_activity extends completion_criteria {
     public function cron() {
         global $DB;
 
+        //Get last time this cron task ran
+        $task = $DB->get_record('task_scheduled', array('classname' => '\core\task\completion_regular_task'));
+
         // Get all users who meet this criteria
         $sql = '
             SELECT DISTINCT
@@ -212,39 +215,28 @@ class completion_criteria_activity extends completion_criteria {
                 cr.id AS criteriaid,
                 ra.userid AS userid,
                 mc.timemodified AS timecompleted
-            FROM
-                {course_completion_criteria} cr
-            INNER JOIN
-                {course} c
-             ON cr.course = c.id
-            INNER JOIN
-                {context} con
-             ON con.instanceid = c.id
-            INNER JOIN
-                {role_assignments} ra
-              ON ra.contextid = con.id
-            INNER JOIN
-                {course_modules_completion} mc
-             ON mc.coursemoduleid = cr.moduleinstance
-            AND mc.userid = ra.userid
-            LEFT JOIN
-                {course_completion_crit_compl} cc
-             ON cc.criteriaid = cr.id
-            AND cc.userid = ra.userid
-            WHERE
-                cr.criteriatype = '.COMPLETION_CRITERIA_TYPE_ACTIVITY.'
-            AND con.contextlevel = '.CONTEXT_COURSE.'
-            AND c.enablecompletion = 1
-            AND cc.id IS NULL
-            AND (
-                mc.completionstate = '.COMPLETION_COMPLETE.'
-             OR mc.completionstate = '.COMPLETION_COMPLETE_PASS.'
-             OR mc.completionstate = '.COMPLETION_COMPLETE_FAIL.'
+            FROM {course_modules_completion} mc
+            INNER JOIN {course_completion_criteria} cr ON mc.coursemoduleid = cr.moduleinstance
+                AND cr.criteriatype = '.COMPLETION_CRITERIA_TYPE_ACTIVITY.'
+            INNER JOIN {course} c ON c.id = cr.course
+                AND c.enablecompletion = 1
+            INNER JOIN {context} con ON con.instanceid = c.id
+                AND con.contextlevel = '.CONTEXT_COURSE.'
+            INNER JOIN {role_assignments} ra ON ra.contextid = con.id
+                AND mc.userid = ra.userid
+            LEFT JOIN {course_completion_crit_compl} cc ON cc.criteriaid = cr.id
+                AND cc.userid = ra.userid
+            WHERE mc.timemodified >= :modified
+                AND (
+                    mc.completionstate = '.COMPLETION_COMPLETE.'
+                    OR mc.completionstate = '.COMPLETION_COMPLETE_PASS.'
+                    OR mc.completionstate = '.COMPLETION_COMPLETE_FAIL.'
                 )
-        ';
+                AND cc.id IS NULL';
 
+        $params = array('modified' => $task->lastruntime);
         // Loop through completions, and mark as complete
-        $rs = $DB->get_recordset_sql($sql);
+        $rs = $DB->get_recordset_sql($sql, $params);
         foreach ($rs as $record) {
             $completion = new completion_criteria_completion((array) $record, DATA_OBJECT_FETCH_BY_KEY);
             $completion->mark_complete($record->timecompleted);
